f9e4619c2b688dd8e11349ab4f10c1f468271df2
Namira-islam <namira.islam212004@gmail.com>
2025-08-27 19:55:15 +0000

    lab1

---
+++ lab1_islam.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    cout << "whats your name ???\n";    
+    cin>> name;
+    cout<<" your name is "<<name;
+    return 0;
+}
