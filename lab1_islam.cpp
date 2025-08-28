17e7f0b6ac5d8e7f208a393480a342b67c70f384
Namira-islam <namira.islam212004@gmail.com>
2025-08-28 15:11:37 +0000

    lab1

--- lab1_islam.cpp
+++ lab1_islam.cpp
@@ -1,9 +1,25 @@
+// Namira Islam 
+// lab 1
 #include <iostream>
 using namespace std;
-int main() {
-    string name;
-    cout << "whats your name ???\n";    
-    cin>> name;
-    cout<<" your name is "<<name;
+int main(){
+    string country;
+    char gender;
+    char percentage;
+    int zipcode;
+    double rate;
+    cout<<"Enter the name of your country:\n";
+    cin>> country;
+    cout<<"Enter your gender:\n";
+    cin>>gender;
+    cout<<"Enter your zipcode:\n";
+    cin>>zipcode;
+    cout<<"Enter the tax rates:\n";
+    cin>> rate;
+    cout<<"Entered country: "<< country;
+    cout<<"\nSelected gender: "<< gender;
+    cout<<"\nZip code: "<< zipcode;
+    cout<<"\nTax rate: "<< rate<<"%";
+
     return 0;
 }
