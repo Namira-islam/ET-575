a6ee7e5a3715910c96c628a438d54f236acb9716
Namira-islam <namira.islam212004@gmail.com>
2025-10-20 02:30:51 +0000

    hw 4

---
+++ homework_4.cpp
@@ -0,0 +1,82 @@
+// namira islam
+// hw 4
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "------------ Question 1 ------------" << endl;
+
+    do {
+        cout << "Enter a number: ";
+        cin >> number;
+
+        if (number < 10) {
+            cout << number << " is invalid! Please enter a number greater than 10: ";
+            cin >> number;
+        }
+    } while (number < 10);
+
+    cout << number << " is a valid number!" << endl;
+
+int num1, num2, min, max;
+
+    cout << "-------- Question 2 --------" << endl;
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+
+    // Determine min and max using if-else
+    if (num1 < num2) {
+        min = num1;
+        max = num2;
+    } else {
+        min = num2;
+        max = num1;
+    }
+
+    cout << "RESULT = ";
+    for (int i = min; i <= max; i++) {
+        cout << i << " ";
+    }
+    cout << endl;                                                 
+
+
+
+// Global dimension
+
+
+
+
+int dimension= 11;
+
+
+    cout << "--------- Question 3 ---------" << endl;
+
+
+   
+        for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+            // draw lowercase 'r' shape
+            if (
+                (j == 3 && i >= 3 && i <= 8) ||            
+                (i == 3 && j >= 3 && j <= 6) ||           
+                (i == 4 && j == 6) ||                      
+                (i == 5 && j == 5) ||                    
+                (i == 6 && j == 6)                         
+            )
+                cout << "# ";
+            else
+                cout << ". ";
+        }
+        cout << endl;
+    }
+
+    
+
+    return 0;
+}
